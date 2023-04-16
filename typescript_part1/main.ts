5a019e74973b51f4e0feca6d79a8fa06aaee59fb
fai-aher <f.hernandezt@uniandes.edu.co>
2023-04-16 13:09:48 -0500

    Series Information WebPage Completed

--- typescript_part1/main.ts
+++ typescript_part1/main.ts
@@ -5,17 +5,33 @@ import { series } from "./data.js";
 
 const seriesTbody: HTMLElement = document.getElementById('Series')!; // tbody node with the name 'Series'
 const avgTbody: HTMLElement = document.getElementById('avgSeasons')!; // tbody node with the name 'avgSeasons'
+const card : HTMLElement = document.getElementById('serie-card')!; // tbody node with the name 'serie-card'
+
+
+/* Function that puts the series information in the table format */
 
 function renderSeriesInTable(series: Serie[]): void {
   series.forEach(c => {
     let trElement = document.createElement("tr");
     trElement.innerHTML = `<td>${c.id}</td>
-                           <td>${c.name}</td>
+                           <td class="serie-name" data-id="${c.id}"><a>${c.name}</a></td>
                            <td>${c.platform}</td>
                            <td>${c.seasonsNumber}</td>`;
     seriesTbody.appendChild(trElement);
   });
+
+    // Addition of click event-listeners for the series name
+    const serieNameCells = document.querySelectorAll('.serie-name')!;
+    serieNameCells.forEach(cell => {
+      cell.addEventListener('click', () => {
+        const serieId = cell.getAttribute('data-id')!;
+        const serie = series.find(serie => serie.id === parseInt(serieId))!;
+        showSeriesDetail(serie);
+      });
+    });
 }
+
+/* Function used to calculate and return the current average number of seaons in all series */
 
 function getAvgSeasons(series: Serie[]): number {
     let avgSeasons: number = 0;
@@ -26,7 +42,20 @@ function getAvgSeasons(series: Serie[]): number {
     });
     
     return avgSeasons/totalSeries;
+
 } 
+
+/* Function to update information of a series into the information card */
+
+function showSeriesDetail(serie: Serie): void {
+  const seriesCard: HTMLElement = document.getElementById('serie-card')!; // card node with the name 'seriesCard'
+  seriesCard.querySelector('.card-title')!.textContent = serie.name;
+  seriesCard.querySelector('.card-text')!.textContent = serie.review;
+  seriesCard.querySelector('.card-img-top')!.setAttribute('src', serie.image);
+  seriesCard.querySelector('.btn')!.setAttribute('href', serie.url);
+}
+
+/* Main function that initializes all other functions */
 
 window.addEventListener("load", () => {
     renderSeriesInTable(series);
@@ -36,3 +65,4 @@ window.addEventListener("load", () => {
     pElement.innerHTML = description +avgSeasons;
     avgTbody.appendChild(pElement);
   });
+
